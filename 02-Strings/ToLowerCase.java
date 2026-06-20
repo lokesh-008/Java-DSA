5bcf8316c1e587a4f32da16ebc36c60735e806cd
Lokesh Patil <lokeshthakare268@gmail.com>
2026-06-20 23:14:37 +0530

    Added LeetCode 709 - To Lower Case

---
+++ 02-Strings/ToLowerCase.java
@@ -0,0 +1,23 @@
+/*
+Problem:
+LeetCode 709 - To Lower Case
+
+Link:
+https://leetcode.com/problems/to-lower-case/
+
+Approach:
+Use Java's built-in toLowerCase() method
+to convert all uppercase characters to lowercase.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+    public String toLowerCase(String s) {
+
+        String ans = s.toLowerCase();
+
+        return ans;
+    }
+}
